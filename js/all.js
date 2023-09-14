c6e825fabc04b7ffe9b356441f515f0f02ff9723
RitaLing1021 <106168868+RitaLing1021@users.noreply.github.com>
2023-09-14 15:36:07 +0800

    DOM練習

--- js/all.js
+++ js/all.js
@@ -1,3 +1,42 @@
+let mouseFamily = {
+  mouseName:['Lv.0死老鼠','Lv.1國王','Lv.2皇后','Lv.3城堡','Lv.4主教','Lv.5騎士','Lv.6八兵','Lv.7砲灰'],
+  author:['宜玲','小薰'],
+  mouseCash:[10,30,40,50,60,70,80,90,100],
+  myCash:37000,
+  todayWork: function(){
+    console.log( mouseFamily.author[0] + '今天有上班喔！');
+    console.log( mouseFamily.author[1] + '今天有上班喔！');
+  },
+  mouseTotal: function(){
+    let cash =  mouseFamily.myCash - mouseFamily.mouseCash[0];
+    console.log( cash + '元');
+  }
+};
+mouseFamily.todayWork();
+mouseFamily.mouseTotal();
+
+
+mouseFamilyTotal = mouseFamily.mouseName.length;
+
+document.getElementById('mouseNum').textContent = mouseFamilyTotal;
+document.getElementById('lv0').textContent = mouseFamily.mouseName[0];
+document.getElementById('lv1').textContent = mouseFamily.mouseName[1];
+document.getElementById('lv2').textContent = mouseFamily.mouseName[2];
+document.getElementById('lv3').textContent = mouseFamily.mouseName[3];
+document.getElementById('lv4').textContent = mouseFamily.mouseName[4];
+document.getElementById('lv5').textContent = mouseFamily.mouseName[5];
+document.getElementById('lv6').textContent = mouseFamily.mouseName[6];
+document.getElementById('lv7').textContent = mouseFamily.mouseName[7];
+
+let house = [
+  { family:'立言'
+},
+  { family:'宜玲'
+}
+]
+
+console.log(house[1].family);
+
 $(document).ready(function() {
 
     $('.top a').click(function(event) {
@@ -8,3 +47,23 @@ $(document).ready(function() {
     });
    
    });
+
+let guestAge = 24;
+    guestGender = true;
+    guestClothes = true;
+
+let past = guestAge >= 18 && guestGender == true && guestClothes == true ;
+
+console.log(past);
+
+let titleH1 = document.querySelector('.title a');
+titleH1.setAttribute('href','http://google.com');
+
+let titleH1id = document.querySelector('.title');
+let titleH1text = "<h1 class='titleH1'>族譜非官方文件</h1>"
+titleH1id.innerHTML = titleH1text;
+
+let titleLi = document.querySelector('.list');
+let link = 'http://www.yahoo.com.tw';
+let titleslogan = '老鼠老虎傻傻分不清楚';
+titleLi.innerHTML ='<li><a href="'+link+'">'+titleslogan+'</a></li>';
---
+++ js/mouseAll.js
@@ -0,0 +1,54 @@
+let mouseSystem = [
+    {
+        mouseName:'Lv.0死老鼠',
+        mouseVariety:'已滅絕',
+        number:0,
+
+    },
+    {
+        mouseName:'Lv.1國王',
+        mouseVariety:'胡蒂亞鼠(世界上數量最少的鼠)',
+        number:3,
+    },
+    {
+        mouseName:'Lv.2皇后',
+        mouseVariety:'食蛇鼠(最凶猛齧齒類動物)',
+        number:5,
+    },
+    {
+        mouseName:'Lv.3城堡',
+        mouseVariety:'黑鼠(啊我就家鼠)',
+        number:8,
+    },
+    {
+        mouseName:'Lv.4主教',
+        mouseVariety:'象鼩(速度比獵豹快三倍)',
+        number:4,
+    },
+    {
+        mouseName:'Lv.5騎士',
+        mouseVariety:'艾力根鼠(又名紳士鼠)',
+        number:5,
+    },
+    {
+        mouseName:'Lv.6八兵',
+        mouseVariety:'黃胸鼠、水家鼠、巢鼠、田鼠、沙鼠、倉鼠、麝鼠、鼢鼠...等等等',
+        number:10,
+    },
+    {
+        mouseName:'Lv.7砲灰',
+        mouseVariety:'褐家鼠(名褐鼠、大鼠、挪威鼠、大家鼠、白尾吊、耗子、糞鼠、溝鼠)',
+        number:20,
+    }
+];
+
+let mouseAll = document.querySelector('.list');
+let mouseLength = mouseSystem.length;
+let mouseTitle = '';
+for(i=0;i<mouseLength;i++){
+    if(mouseSystem[i].number>4){
+    let content = '<li>'+ mouseSystem[i].mouseName +'</li>';
+    mouseTitle +=content;
+}
+}
+mouseAll.innerHTML = mouseTitle;
